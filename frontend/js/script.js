eeef7935872f9856179613669b52387f67d61306
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2020-11-17 21:33:21 +0000

    added div into alert box

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,23 +1,58 @@
 //initialising calendar
 
- var timeDropDown;
+ var timeDropDown = document.getElementById('myDIV').innerHTML;
+ function test(){
+   console.log('test');
+ }
+ test();
+//  $(".timepicker").wickedpicker({
+	
+//   twentyFour: false,  //Display 24 hour format, defaults to false
+//   upArrow: 'wickedpicker__controls__control-up',  //The up arrow class selector to use, for custom CSS
+//   downArrow: 'wickedpicker__controls__control-down', //The down arrow class selector to use, for custom CSS
+//   close: 'wickedpicker__close', //The close class selector to use, for custom CSS
+//   hoverState: 'hover-state', //The hover state class to use, for custom CSS
+//   title: 'Timepicker', //The Wickedpicker's title,
+//   showSeconds: false, //Whether or not to show seconds,
+//   timeSeparator: ' : ', // The string to put in between hours and minutes (and seconds)
+//   secondsInterval: 1, //Change interval for seconds, defaults to 1,
+//   minutesInterval: 1, //Change interval for minutes, defaults to 1
+//   beforeShow: null, //A function to be called before the Wickedpicker is shown
+//   afterShow: null, //A function to be called after the Wickedpicker is closed/hidden
+//   show: null, //A function to be called when the Wickedpicker is shown
+//   clearable: true, //Make the picker's input clearable (has clickable "x")
+
+// });
   $('#calendar').fullCalendar({
     firstDay: 1, //setting first day to monday instead of default 0, sunday
     dayClick: function(date, jsEvent, view) {
         // $('.timepicker').pickatime()
         //when you click on the day, it will display 
-      alert("Selected Date:"+date._d)
+      alert("Selected Date:"+date._d + timeDropDown)
+      // alert("Times available"+(document.getElementById('myDIV').value))
     //   timeDropDown();
      }});
-
-
-     new Picker(document.querySelector('.js-time-picker'), {
-        format: 'HH:mm',
-        headers: true,
-        text: {
-          title: 'Pick a time',
-        },
-      });
+   
+    //  function myFunction() {
+    //   var x = document.getElementById("myDIV");
+    //   if (x.style.display === "none") {
+    //     x.style.display = "block";
+    //   } else {
+    //     x.style.display = "none";
+    //   }
+    // }
+    //  new Picker(document.querySelector('.js-time-picker'), {
+    //     format: 'HH:mm',
+    //     headers: true,
+    //     text: {
+    //       title: 'Pick a time',
+    //     },
+    //   });
+  //   (function($) {
+  //     $(function() {
+  //         $('input.timepicker').timepicker();
+  //     });
+  // });
     
 // (function($) {
 //     $(function() {
